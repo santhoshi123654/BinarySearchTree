a0486c1c5dd11c7e42151f34d269e2c7d4515504
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-21 18:42:53 +0530

    Added: INode Program

---
+++ src/com/bridgelabz/INode.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public interface INode<K> {
+	
+	int data = 0;
+	Object left = null;
+	K getKey();
+	void setkey(K key);
+	INode<K> getNext();
+	void setNext(INode<K> next);
+}
